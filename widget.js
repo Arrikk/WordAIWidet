0ef313c663fe184952a73a9b201c7986622365f4
Arrikk <70144434+Arrikk@users.noreply.github.com>
2023-11-30 15:42:29 +0100

    Update widget.js

--- widget.js
+++ widget.js
@@ -15,6 +15,7 @@ chatButton.style.padding = "10px";
 chatButton.style.cursor = "pointer";
 chatButton.style.fontSize = "20px"
 chatButton.style.boxShadow = "rgb(110 104 104 / 38%) 0px 4px 10px"
+chatButton.style.zIndex= "1000"
 
 // Append the button to the body
 document.body.appendChild(chatButton);
